4757840d045615df58599306684ccbcfc443a326
4o66 <throwarock@gmail.com>
2015-06-03 23:09:41 -0700

    Copied in Template Plugin to start making Launch Options Plugin

---
+++ KSPModAdmin.Plugin.LaunchOptions/KSPMAPlugin.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using KSPModAdmin.Core;
+using KSPModAdmin.Core.Utils;
+using KSPModAdmin.Plugin.LaunchOptions.Properties;
+using KSPModAdmin.Plugin.LaunchOptions.Views;
+
+namespace KSPModAdmin.Plugin.LaunchOptions
+{
+    /// <summary>
+    /// KSP MA plugin container class.
+    /// </summary>
+    public class KSPMAPlugin : IKSPMAPlugin
+    {
+        private TabView[] mMainTabViews = null;
+        private TabView[] mOptionTabViews = new List<TabView>().ToArray(); // Initialize with an empty array if not needed.
+
+
+        /// <summary>
+        /// Name of the plugin.
+        /// </summary>
+        public string Name { get { return "Generic Plugin"; } }
+
+        /// <summary>
+        /// Description of the plugin.
+        /// </summary>
+        public string Description
+        {
+            get
+            {
+                return "This Plugin adds a tab to [insert purpose here].";
+            }
+        }
+
+        /// <summary>
+        /// Array of TabViews that the mod imports.
+        /// </summary>
+        public TabView[] MainTabViews
+        {
+            get
+            {
+                if (mMainTabViews == null)
+                    mMainTabViews = new[] { new TabView(new Guid("{13FC3AEC-83C4-4BF3-948C-209A9043AC04}"), new ucPluginView(), Resources.Unknown) };
+
+                return mMainTabViews;
+            }
+        }
+
+        /// <summary>
+        /// Array of OptionTabViews that the mod imports.
+        /// </summary>
+        public TabView[] OptionTabViews
+        {
+            get
+            {
+                return mOptionTabViews;
+            }
+        }
+    }
+}
